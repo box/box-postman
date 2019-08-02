075f2c740544e16de22d92ad0878f3dcf3aa3b00
Cristiano Betta <cbetta@box.com>
2019-08-02 13:53:44 +0100

    Empty collection before pusing new collection

--- src/scripts/release.js
+++ src/scripts/release.js
@@ -9,6 +9,23 @@ const release = async (locale = process.argv[1]) => {
   const collection = JSON.parse(fs.readFileSync(`${OUTPUT_FOLDER}/collection.${locale}.json`).toString())
   const collectionId = process.env[`${locale.toUpperCase()}_POSTMAN_COLLECTION_ID`]
 
+  // prevent old folders from remaining in place by first removing all items
+  const emptyCollection = { ...collection }
+  emptyCollection.item = []
+
+  // first publish an empty collection to ensure all folders are removed
+  await axios.put(
+    `https://api.getpostman.com/collections/${collectionId}`,
+    JSON.stringify({ collection: emptyCollection }),
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Api-Key': process.env.POSTMAN_API_KEY
+      }
+    }
+  ).catch(error => console.dir(error.response, { depth: 100 }))
+
+  // then publish the new collection
   await axios.put(
     `https://api.getpostman.com/collections/${collectionId}`,
     JSON.stringify({ collection }),
@@ -18,7 +35,7 @@ const release = async (locale = process.argv[1]) => {
         'X-Api-Key': process.env.POSTMAN_API_KEY
       }
     }
-  )
+  ).catch(error => console.dir(error.response, { depth: 100 }))
 }
 
 const releaseAll = async () => {
