036bbfb4f6232eddc6fbb666b2b4c273ccdf089a
barduinor <barduinor@gmail.com>
2023-09-15 17:10:04 -0400

    fix release to work the same as the original

--- src/scripts/release.js
+++ src/scripts/release.js
@@ -6,10 +6,10 @@ const OUTPUT_FOLDER = './compiled'
 
 const release = async (locale = process.argv[1]) => {
   const collection = JSON.parse(fs.readFileSync(`${OUTPUT_FOLDER}/collection.${locale}.json`).toString())
-  const privateRemoteCollectionID = process.env[`PRIVATE_${locale.toUpperCase()}_POSTMAN_COLLECTION_ID`]
+  // const privateRemoteCollectionID = process.env[`PRIVATE_${locale.toUpperCase()}_POSTMAN_COLLECTION_ID`]
   const publicRemoteCollectionID = process.env[`PUBLIC_${locale.toUpperCase()}_POSTMAN_COLLECTION_ID`]
 
-  oldDeploy.oldDeployBulk(privateRemoteCollectionID, collection, publicRemoteCollectionID)
+  oldDeploy.oldDeployBulk(publicRemoteCollectionID, collection, publicRemoteCollectionID)
 }
 
 const releaseAll = async () => {
